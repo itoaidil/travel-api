81e8466a70ef8e1c499d336fdac6bc7cb71b8242
fitro <fitroaidil@fitros-MacBook-Air.local>
2026-01-05 21:09:42 +0700

    Fix: Remove invalid db import from bookingRoutes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
 const { sendNotificationToMultipleDrivers } = require('../services/notificationService');
 
 /**
